3259bea6377857b79e6e3e712193c9a70f7ae7e5
JoaquimGouveia <joaquimgouveia2000@gmail.com>
2022-03-04 15:13:33 +0100

    Update SudokuSolverGUI.java

--- src/sudoku/SudokuSolverGUI.java
+++ src/sudoku/SudokuSolverGUI.java
@@ -1,5 +1,29 @@
 package sudoku;
 
+import java.awt.Container;
+import java.awt.GridLayout;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
+
 public class SudokuSolverGUI {
-	System.out.println("hej");
+	
+	public SudokuSolverGUI(SudokuSolver s) {
+		SwingUtilities.invokeLater(() -> createWindow(s, "Joaquim och Olles SudokuSolver", 800, 800));
+	}
+	
+	public void createWindow(SudokuSolver sudoku, String title, int width, int height) {
+		JFrame frame = new JFrame(title);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		Container container = frame.getContentPane();
+		JPanel sudokuPanel = new JPanel();
+		JPanel buttonPanel = new JPanel();
+		JButton solveButton = new JButton("Solve Sudoku");
+		JButton clearButton = new JButton("Clear Sudoku");
+		GridLayout sudokuGrid = new GridLayout(9, 9);
+		
+	}
 }
